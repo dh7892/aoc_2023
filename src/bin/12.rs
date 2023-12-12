57b9bcaf1c55e46fef93ec104bf8dde359b73390
Dave <dave@dhills.net>
2023-12-12 23:20:43 +0000

    Refactor to remove option

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -6,41 +6,41 @@ use std::collections::HashMap;
 fn possible_ways(
     cache: &mut HashMap<(usize, usize, usize), usize>,
     s: &[u8],
-    within: Option<usize>,
+    within: usize,
     remaining: &[usize],
 ) -> usize {
     if s.is_empty() {
         return match (within, remaining.len()) {
-            (None, 0) => 1,
-            (Some(x), 1) if x == remaining[0] => 1,
+            (0, 0) => 1,
+            (x, 1) if x == remaining[0] => 1,
             _ => 0,
         };
     }
-    if within.is_some() && remaining.is_empty() {
+    if within > 0 && remaining.is_empty() {
         return 0;
     }
 
-    let key = (s.len(), within.unwrap_or(0), remaining.len());
+    let key = (s.len(), within, remaining.len());
     if let Some(&x) = cache.get(&key) {
         return x;
     }
 
     let ways = match (s[0], within) {
-        (b'.', Some(x)) if x != remaining[0] => 0,
-        (b'.', Some(_)) => possible_ways(cache, &s[1..], None, &remaining[1..]),
-        (b'.', None) => possible_ways(cache, &s[1..], None, remaining),
-        (b'#', Some(_)) => possible_ways(cache, &s[1..], within.map(|x| x + 1), remaining),
-        (b'#', None) => possible_ways(cache, &s[1..], Some(1), remaining),
-        (b'?', Some(x)) => {
-            let mut ans = possible_ways(cache, &s[1..], within.map(|x| x + 1), remaining);
+        (b'.', 0) => possible_ways(cache, &s[1..], 0, remaining),
+        (b'.', x) if x != remaining[0] => 0,
+        (b'.', _) => possible_ways(cache, &s[1..], 0, &remaining[1..]),
+        (b'#', 0) => possible_ways(cache, &s[1..], 1, remaining),
+        (b'#', x) => possible_ways(cache, &s[1..], x + 1, remaining),
+        (b'?', 0) => {
+            possible_ways(cache, &s[1..], 1, remaining)
+                + possible_ways(cache, &s[1..], 0, remaining)
+        }
+        (b'?', x) => {
+            let mut ans = possible_ways(cache, &s[1..], x + 1, remaining);
             if x == remaining[0] {
-                ans += possible_ways(cache, &s[1..], None, &remaining[1..])
+                ans += possible_ways(cache, &s[1..], 0, &remaining[1..])
             }
             ans
-        }
-        (b'?', None) => {
-            possible_ways(cache, &s[1..], Some(1), remaining)
-                + possible_ways(cache, &s[1..], None, remaining)
         }
         _ => unreachable!(),
     };
@@ -59,7 +59,7 @@ pub fn part_one(input: &str) -> Option<u32> {
                 .map(|w| w.parse::<usize>().unwrap())
                 .collect::<Vec<_>>();
             cache.clear();
-            let p1 = possible_ways(&mut cache, vents.as_bytes(), None, &nums);
+            let p1 = possible_ways(&mut cache, vents.as_bytes(), 0, &nums);
             p1 as u32
         })
         .sum();
@@ -79,7 +79,7 @@ pub fn part_two(input: &str) -> Option<u64> {
             let new_vents = (0..5).map(|_| vents).join("?");
             let new_nums = (0..5).flat_map(|_| &nums).copied().collect::<Vec<_>>();
             cache.clear();
-            let p2 = possible_ways(&mut cache, new_vents.as_bytes(), None, &new_nums);
+            let p2 = possible_ways(&mut cache, new_vents.as_bytes(), 0, &new_nums);
             p2 as u64
         })
         .sum();
